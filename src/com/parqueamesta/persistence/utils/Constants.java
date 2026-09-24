38a9ef93abc5774a5ab221e3f30f1dade1594b86
Nestor Saumeth <nisaumeth@unimagdalena.edu.co>
2026-09-24 00:54:03 -0500

    Update: Constants now merged with DB.

--- src/com/parqueamesta/persistence/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.parqueamesta.persistence.utils;
-
-public record Constants() {
-    public static final String DB_URL = "jdbc:postgresql://localhost:5432/demo_parquadero";
-    public static final String DB_USER = "postgres";
-    public static final String DB_PASSWORD = "ArqTests262";
-}
